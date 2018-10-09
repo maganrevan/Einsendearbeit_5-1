68f65e9763e0145c9c722941e1e9246d4dbb1f29
kruschwitzm <magnus.kruschwitz@i12.de>
2018-10-09 19:03:21 +0200

    Initial Commit

---
+++ Einsendearbeit_5-1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Einsendearbeit_5-1
+//
+//  Created by Magnus Kruschwitz on 09.10.18.
+//  Copyright © 2018 Magnus Kruschwitz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
